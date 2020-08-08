4ec6532c782fd4d9680a21ea88e9094b70133d9e
Colin Tan <colintan95@gmail.com>
2020-08-07 17:36:07 -0700

    Changed GLFW from git submodule to dependency without version control.

--- src/gfx/hello_triangle/main.cpp
+++ src/gfx/hello_triangle/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <cstdlib>
+
+#include <GLFW/glfw3.h>
 
 int main() {
+  if (!glfwInit()) {
+    std::cerr << "Could not initialize GLFW." << std::endl;
+    exit(1);
+  }
+
   std::cout << "Hello, World!" << std::endl;
+
+  glfwTerminate();
 
   return 0;
 }
