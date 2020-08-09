45eeec097fc558885f7e5d058b4cd9c8b8c6b8bf
Colin Tan <colintan95@gmail.com>
2020-08-08 21:17:09 -0700

    Added binding of material id vertex data.

--- src/gfx/global_illum/main.cpp
+++ src/gfx/global_illum/main.cpp
@@ -193,6 +193,10 @@ void RenderPass() {
     glEnableVertexAttribArray(2);
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
+    glBindBuffer(GL_ARRAY_BUFFER, gl_mtl_id_vbos[i]);
+    glEnableVertexAttribArray(3);
+    glVertexAttribPointer(3, 1, GL_INT, GL_FALSE, 0, 0);
+
     glDrawArrays(GL_TRIANGLES, 0, mesh.num_verts);
   }
 }
